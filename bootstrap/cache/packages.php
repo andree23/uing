10a8f66c8dd453802ebd8f7fdbd29dfa12b69d46
andree23 <andriyansyahmaaz@gmail.com>
2020-09-18 05:59:30 +0700

    2

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -34,4 +34,11 @@
       0 => 'Carbon\\Laravel\\ServiceProvider',
     ),
   ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
 );
